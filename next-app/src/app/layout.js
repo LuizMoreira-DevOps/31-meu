069e995ec9b8cbab3dc49ff101d17fc1869e0e5f
Nando-DevOps <luiz.moreira1@pucpr.edu.br>
2026-09-22 13:31:08 -0300

    feat: initialize Next.js base with static rendering (#41)
    Refs #40

---
+++ next-app/src/app/layout.js
@@ -0,0 +1,28 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "@/css/global.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "31 Meu Buffet Infantil",
+  description: "Buffet Infantil em Curitiba para festas, aniversários e momentos especiais.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+        <html
+      lang="pt-BR"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+    >
+      <body>{children}</body>
+    </html>
+  );
+}
