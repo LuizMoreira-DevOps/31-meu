e4a7f34d165bfdd22c6d87185ee0ea4dac986c3a
LuizMoreira-DevOps <luiz.moreira1@pucpr.edu.br>
2026-09-23 12:34:21 -0300

    feat(pacotes): adiciona página provisória de combos
    Refs #44

--- next-app/src/app/layout.js
+++ next-app/src/app/layout.js
@@ -2,27 +2,28 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "@/css/global.css";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata = {
-  title: "31 Meu Buffet Infantil",
-  description: "Buffet Infantil em Curitiba para festas, aniversários e momentos especiais.",
+    title: "31 Meu Buffet Infantil",
+    description:
+        "Buffet Infantil em Curitiba para festas, aniversários e momentos especiais.",
 };
 
 export default function RootLayout({ children }) {
-  return (
+    return (
         <html
-      lang="pt-BR"
-      className={`${geistSans.variable} ${geistMono.variable}`}
-    >
-      <body>{children}</body>
-    </html>
-  );
+            lang="pt-BR"
+            className={`${geistSans.variable} ${geistMono.variable}`}
+        >
+            <body>{children}</body>
+        </html>
+    );
 }
